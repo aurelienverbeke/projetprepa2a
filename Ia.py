5e69afcf8038b49d8e94136b94f5075b0442b32e
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2023-12-04 18:10:18 +0100

    Create Ia.py

---
+++ Ia.py
@@ -0,0 +1,16 @@
+class Ia:
+  def __init__(self, niveau):
+    self.niveau = niveau
+
+  def calcul_coup(self, plateau, id_joueur,nb_carte_jouees):
+    if nb_carte_jouees == 0 and not self.peut_jouer():
+      
+
+  def defausse(self):
+    pass
+
+  def pioche(self):
+    pass
+
+  def contre(self):
+    pass
