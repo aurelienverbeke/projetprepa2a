0c4e529fb83b072444cc0f1faf2c1d7c1cc264a1
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-12-04 18:20:37 +0100

    Première version de la méthode jouer (sans aucune sécuritée)

--- Rdr_jeu.py
+++ Rdr_jeu.py
@@ -27,6 +27,40 @@ class RoiDuRing:
 
         self.taille = taille
 
+    # actionJoue tuple (type, liste carte(s), caseCible)
+    # caseCible tuple (ligne,colonne)
+    # liste de carte(s) pour defausse ou coup bas
+    # type : 0 (coup bas), 1 (joker), 2 (attaque poussee),
+    # 3 (attaque endurance), 4 (mouvement), 5 (fin=rien)
+    def jouer(self, idJoueur, actionJoue):
+        if actionJoue[0] == 0:
+            self.joueurs[idJoueur].retirer_cartes(actionJoue[1])
+        elif actionJoue[0] == 1:
+            joueurCible = self.joueur_de_case(actionJoue[2])
+            carteVolee = self.joueurs[joueurCible].main[0]
+            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+            self.joueurs[joueurCible].retirer_carte([carteVolee])
+            self.joueurs[idJoueur].ajouter_carte([carteVolee])
+
+        elif actionJoue[0] == 2:
+            joueurCible = self.joueur_de_case(actionJoue[2])
+            positionJoueurCible = self.joueurs[joueurCible].position
+            positionJoueur = self.joueurs[idJoueur].position
+            deltaPosition = (positionJoueurCible[0]-positionJoueur[0], positionJoueurCible[1]-positionJoueur[1])
+            self.joueurs[joueurCible].position = (positionJoueurCible[0] + deltaPosition[0], positionJoueurCible[1] + deltaPosition[1])
+            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+
+        elif actionJoue[0] == 3:
+            joueurCible = self.joueur_de_case(actionJoue[2])
+            vieRetiree = 1
+            if self.joueurs[idJoueur].position == (0, 0):
+                vieRetiree = 2
+            self.joueurs[joueurCible].retirer_vie(vieRetiree)
+            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+
+        elif actionJoue[0] == 4:
+            self.joueurs[idJoueur].position = actionJoue[2]
+            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
 
 
     def joueur_de_case(self,position):
