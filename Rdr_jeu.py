c07fa56ff24e731e904273d69f708b7600f12c78
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-12-04 18:22:53 +0100

    Correction du nom des methodes²

--- Rdr_jeu.py
+++ Rdr_jeu.py
@@ -38,9 +38,9 @@ class RoiDuRing:
         elif actionJoue[0] == 1:
             joueurCible = self.joueur_de_case(actionJoue[2])
             carteVolee = self.joueurs[joueurCible].main[0]
-            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
-            self.joueurs[joueurCible].retirer_carte([carteVolee])
-            self.joueurs[idJoueur].ajouter_carte([carteVolee])
+            self.joueurs[idJoueur].retirer_cartes(actionJoue[1])
+            self.joueurs[joueurCible].retirer_cartes([carteVolee])
+            self.joueurs[idJoueur].ajouter_cartes([carteVolee])
 
         elif actionJoue[0] == 2:
             joueurCible = self.joueur_de_case(actionJoue[2])
@@ -48,7 +48,7 @@ class RoiDuRing:
             positionJoueur = self.joueurs[idJoueur].position
             deltaPosition = (positionJoueurCible[0]-positionJoueur[0], positionJoueurCible[1]-positionJoueur[1])
             self.joueurs[joueurCible].position = (positionJoueurCible[0] + deltaPosition[0], positionJoueurCible[1] + deltaPosition[1])
-            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+            self.joueurs[idJoueur].retirer_cartes(actionJoue[1])
 
         elif actionJoue[0] == 3:
             joueurCible = self.joueur_de_case(actionJoue[2])
@@ -56,11 +56,11 @@ class RoiDuRing:
             if self.joueurs[idJoueur].position == (0, 0):
                 vieRetiree = 2
             self.joueurs[joueurCible].retirer_vie(vieRetiree)
-            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+            self.joueurs[idJoueur].retirer_cartes(actionJoue[1])
 
         elif actionJoue[0] == 4:
             self.joueurs[idJoueur].position = actionJoue[2]
-            self.joueurs[idJoueur].retirer_carte(actionJoue[1])
+            self.joueurs[idJoueur].retirer_cartes(actionJoue[1])
 
 
     def joueur_de_case(self,position):
