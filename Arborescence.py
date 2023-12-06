e6a9dfa6033d981faa619f87f43ae566a42fca96
Aurelien VERBEKE <aurelienv31@gmail.com>
2023-12-06 12:17:36 +0100

    Debut de scoring pour la fonction d'evaluation

--- Arborescence.py
+++ Arborescence.py
@@ -1,14 +1,15 @@
 class Arborescence:
-    def __init__(self, etat="", joueurCourant=0):
+    def __init__(self, etat="", joueurCourant=0, taillePlateau):
         """
         Paramètres:
             - etat (dict) : de la forme {"pioche": list(cartes), "defausse": list(cartes),
-              idJoueur : {"main" : list(cartes), "endurance" : int}}
+              idJoueur : {"main" : list(cartes), "endurance" : int, "position" : tuple(int)}}
         """
         self.etat = etat
         self.joueurCourant = joueurCourant
         self.sousArbres = []
         self.valeur = 0
+        self.taillePlateau = taillePlateau
 
     def generer_fils(self):
         pass
@@ -29,4 +30,32 @@ class Arborescence:
             - (list): pour chaque joueur, son score
                 exemple: [<score joueur0>, <score joueur1>, <score joueur2>]
         """
-        pass
+        
+        scores = [0 for _ in range(len(self.etat) - 2)]
+
+        # on evalue le score pour chaque joueur
+        for idJoueur in len(scores):
+            joueur = self.etat[idJoueur]
+            
+            x = joueur["position"][1]
+            y = joueur["position"][0]
+            extremite = self.taillePlateau/2
+            
+            # le joueur est en position centrale
+            if (x, y) == (0, 0):
+                score[idJoueur] += 3
+            
+            # le joueur est dans un coin
+            if x == -extremite or x == extremite or y == -extremite or y == extremite:
+                score[idJoueur] -= 2
+
+            # on prend en compte l'endurance du joueur
+            score[idJoueur] += joueur["endurance"]
+
+            # le joueur a des cartes d'attaque
+            for carte in joueur["main"]:
+                if carte.motif == "K" or carte.motif == "C":
+                    score[idJoueur] += carte.valeur
+
+
+        return scores
