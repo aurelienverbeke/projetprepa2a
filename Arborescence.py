b89a66148ed84a316903dd5ba9ed5c8f1a004007
Aurelien VERBEKE <aurelienv31@gmail.com>
2023-12-06 11:47:41 +0100

    Arborescence : erreur dans le nom du dictionnaire explore dans est_fini, et harmonisation du constructeur

--- Arborescence.py
+++ Arborescence.py
@@ -1,11 +1,11 @@
 class Arborescence:
-    def __init__(self, positionInitiale="", joueurCourant=0):
+    def __init__(self, etat="", joueurCourant=0):
         """
         Paramètres:
-            - positionInitiale (dict) : de la forme {"pioche": list(cartes), "defausse": list(cartes),
+            - etat (dict) : de la forme {"pioche": list(cartes), "defausse": list(cartes),
               idJoueur : {"main" : list(cartes), "endurance" : int}}
         """
-        self.etat = positionInitiale
+        self.etat = etat
         self.joueurCourant = joueurCourant
         self.sousArbres = []
         self.valeur = 0
@@ -15,8 +15,8 @@ class Arborescence:
 
     def est_fini(self):
         nbVivants = 0
-        for idJoueur in range(len(self.positionInitiale) - 2):
-            if self.positionInitiale[idJoueur]["endurance"] > 0:
+        for idJoueur in range(len(self.etat) - 2):
+            if self.etat[idJoueur]["endurance"] > 0:
                 nbVivants += 1
         return nbVivants <= 1
 
