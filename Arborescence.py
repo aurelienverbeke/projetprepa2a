d2ae3fcf673f0b6dbef072f907b35a766fe6e039
Aurelien VERBEKE <aurelienv31@gmail.com>
2023-12-06 14:56:39 +0100

    On continue d'ameliorer la fonction d'evaluation

--- Arborescence.py
+++ Arborescence.py
@@ -1,3 +1,11 @@
+SCORE_COEFFICIENT_ENDURANCE = 1
+SCORE_COEFFICIENT_NB_CARTES = 1
+SCORE_CARTE_DEPLACEMENT = 1
+SCORE_COEFFICIENT_CARTE_ATTAQUE = 1
+SCORE_POSITION_CENTRALE = 3
+SCORE_POSITION_COIN = -1
+
+
 class Arborescence:
     def __init__(self, nombreCoupParNoeud, taillePlateau, etat="", joueurCourant=0):
         """
@@ -64,20 +72,24 @@ class Arborescence:
 
             # le joueur est en position centrale
             if (x, y) == (0, 0):
-                scores[idJoueur] += 3
+                scores[idJoueur] += SCORE_POSITION_CENTRE
 
             # le joueur est dans un coin
             if x == -extremite or x == extremite or y == -extremite or y == extremite:
-                scores[idJoueur] -= 2
+                scores[idJoueur] += SCORE_POSITION_COIN
 
             # on prend en compte l'endurance du joueur
-            scores[idJoueur] += joueur["endurance"]
+            scores[idJoueur] += SCORE_COEFFICIENT_ENDURANCE * joueur["endurance"]
 
-            # le joueur a des cartes d'attaque
+            # nombre de cartes
+            scores[idJoueur] += SCORE_COEFFICIENT_NB_CARTES * len(joueur[main])
+
             for carte in joueur["main"]:
+                # le joueur a des cartes d'attaque
                 if carte.motif == "K" or carte.motif == "C":
-                    scores[idJoueur] += carte.valeur
-
-            
+                    scores[idJoueur] += SCORE_COEFFICIENT_CARTE_ATTAQUE * carte.valeur
+                # le joueur a des cartes de deplacement
+                if carte.motif == "P" or carte.motif == "T":
+                    scores[idJoueur] += SCORE_CARTE_DEPLACEMENT
 
         return scores
