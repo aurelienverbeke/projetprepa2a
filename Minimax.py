e54dfbb747b72c47efd861dae4fcb645b1a43353
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-12-12 02:07:41 +0100

    Correction d'un bug (quand un joueur meurt, on l'ignore pour le calcul de minimax)

--- Minimax.py
+++ Minimax.py
@@ -1,15 +1,16 @@
 from Arborescence import Arborescence
 
 def minimax(arbre, joueurCourant, pmax):
-    if pmax == 0 or arbre.est_fini():
+    if arbre.est_fini() or pmax == 0:
         valeur = arbre.evaluation()
         arbre.valeur = valeur
         return valeur
-    valeur = [float("-inf") for _ in range(len(arbre.etat) - 2)]
+    valeur = {idJoueur: float("-inf") for idJoueur in arbre.etat["listeJoueurs"]}
     for fils in arbre.generer_fils():
         joueurSuivant = fils.joueurCourant
         valeurFils = minimax(fils, joueurSuivant, pmax-1)
-        valeur = max(valeur, valeurFils, key=lambda x : x[joueurCourant])
+        if joueurCourant in valeurFils.keys():
+            valeur = max(valeur, valeurFils, key=lambda x: x[joueurCourant])
 
     arbre.valeur = valeur
     return valeur
@@ -18,7 +19,9 @@ def choisir_coup(arbre, joueurCourant, pmax):
     minimax(arbre, joueurCourant, pmax)
     meilleurFils = arbre.sousArbres[0]
     for fils in arbre.sousArbres:
-        if fils.valeur[joueurCourant] > meilleurFils.valeur[joueurCourant]:
+        if joueurCourant not in fils.valeur.keys():
+            continue
+        if joueurCourant not in meilleurFils.valeur.keys() or fils.valeur[joueurCourant] > meilleurFils.valeur[joueurCourant]:
             meilleurFils = fils
     return meilleurFils.dernierCoup["cartes"]
 
@@ -26,9 +29,9 @@ def choisir_coup(arbre, joueurCourant, pmax):
 if __name__ == "__main__":
     from Carte import Carte
     etat = {"pioche": [Carte("T", 6), Carte("T", 7), Carte("T", 8)], "listeJoueurs": [0, 1],
-            0: {"main": [Carte("T", 3), Carte("C", 10), Carte("J", 11)], "endurance": 10,
+            0: {"main": [Carte("T", 3), Carte("C", 10), Carte("J", 11)], "endurance": 1,
                 "position": (-2, -2)},
-            1: {"main": [Carte("K", 9), Carte("T", 10)], "endurance": 10,
+            1: {"main": [Carte("T", 10), Carte("K", 9)], "endurance": 1,
                 "position": (-1, -1)}}
     A = Arborescence(5, 5, etat)
     print(choisir_coup(A, 0, 4))
