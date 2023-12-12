9aeac1315e7dd7e218820bb175ca434d425e7213
Raphaël Bourgouin <raph.bourgouin@free.fr>
2023-12-12 16:29:46 +0100

    Changement de l'etat de test (un etat qui actuellement pousse le bot a se suicider)

--- Minimax.py
+++ Minimax.py
@@ -31,10 +31,12 @@ def choisir_coup(arbre, joueurCourant, pmax):
 
 if __name__ == "__main__":
     from Carte import Carte
-    etat = {"pioche": [Carte("T", 6), Carte("T", 7), Carte("T", 8)], "listeJoueurs": [0, 1],
-            0: {"main": [Carte("T", 3), Carte("C", 10), Carte("J", 11)], "endurance": 1,
-                "position": (-2, -2)},
-            1: {"main": [Carte("T", 10), Carte("K", 9)], "endurance": 1,
-                "position": (-1, -1)}}
+    etat = {"pioche": [Carte("T", 8), Carte("C", 10), Carte("P", 8), Carte("T", 9), Carte("P", 9), Carte("C", 11), Carte("P", 10),
+                       Carte("P", 7), Carte("J", 15), Carte("K", 10), Carte("C", 12), Carte("P", 11), Carte("J", 15), Carte("C", 14),
+                       Carte("K", 7), Carte("C", 9), Carte("T", 7), Carte("K", 13), Carte("K", 9), Carte("K", 12)], "listeJoueurs": [0, 1],
+            0: {"main": [Carte("C", 8), Carte("C", 7), Carte("P", 13), Carte("T", 14), Carte("P", 12)], "endurance": 9,
+                "position": (0, 0)},
+            1: {"main": [Carte("C", 13), Carte("K", 8), Carte("K", 14), Carte("K", 11), Carte("T", 10)], "endurance": 10,
+                "position": (1, 2)}}
     A = Arborescence(5, 5, etat)
-    print(choisir_coup(A, 0, 4))
+    print(choisir_coup(A, 0, 1))
